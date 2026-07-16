67bea0278db70a1266abcb023d0ed12e5e02c774
Dani Prados <daniprados@gmail.com>
2026-07-16 11:41:25 +0200

    fix: Update namespace and URLs in UserScript metadata

--- dist/script.user.js
+++ dist/script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Gedac manageer
-// @namespace    
+// @namespace    https://github.com/daniprados/GedacManager
 // @version      1.15.5
 // @description  Millores per a la plataforma Gedac
 // @author       daniprados
@@ -11,8 +11,8 @@
 // @require      https://cdn.jsdelivr.net/npm/html2canvas@1.4.1/dist/html2canvas.min.js
 // @require      https://cdn.jsdelivr.net/npm/jspdf@4.2.1/dist/jspdf.umd.min.js
 // @grant        none
-// @updateURL    
-// @downloadURL  
+// @updateURL    https://github.com/daniprados/GedacManager/raw/refs/heads/main/dist/script.user.js
+// @downloadURL  https://github.com/daniprados/GedacManager/raw/refs/heads/main/dist/script.user.js
 // ==/UserScript==
 
 (()=>{var r=class t{static TARGET_TITLE="Gesti\xF3 presentaci\xF3 documents matr\xEDcula (CFP)";constructor(e){this.logger=e}isTargetPage(e){let a=[...e.querySelectorAll("h4")].some(c=>this.#e(c.textContent)===t.TARGET_TITLE);return this.logger.debug("Comprovaci\xF3 de la p\xE0gina de matr\xEDcula CFP",{isTarget:a}),a}#e(e){return e?.replace(/\s+/g," ").trim()??""}};var o=class{constructor(e,s){this.logger=e,this.pageDetector=s}start(e){return this.pageDetector.isTargetPage(e)?(this.logger.info("Inicialitzaci\xF3 de les eines de matr\xEDcula CFP"),!0):!1}};(()=>{let t=console,e=new r(t);new o(t,e).start(document)})();})();
